a1ae608daadf99adb8636768143a96ba1c095edb
Koichi Takabatake <takabatakekoichi@gmail.com>
2024-02-21 20:40:29 +0900

    lazygit

--- src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
@@ -2,7 +2,8 @@
 
 set -eux
 
-mkdir /root/.config
-cp -R helpers/installers/config-nvim /root/.config/nvim
+su - rails
+mkdir /home/rails/.config
+cp -R helpers/installers/config-nvim /home/rails/.config/nvim
 
 nvim --headless "+Lazy! sync" +qa
