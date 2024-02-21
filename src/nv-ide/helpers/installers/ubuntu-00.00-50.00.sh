4943acc95304e4a0d5bb20eba7b0fd85474010d9
Koichi Takabatake <takabatakekoichi@gmail.com>
2024-02-21 20:40:29 +0900

    lazygit

--- src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
+su - rails
+mkdir /home/rails/.config
+cp -R helpers/installers/config-nvim /home/rails/.config/nvim
+
 mkdir /root/.config
 cp -R helpers/installers/config-nvim /root/.config/nvim
 
