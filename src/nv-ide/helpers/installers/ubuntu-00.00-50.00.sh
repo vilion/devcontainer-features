09e41b268fa2a80f35493c2e2a9cdef85b7cea45
Koichi Takabatake <takabatakekoichi@gmail.com>
2024-02-22 17:21:44 +0900

    初期インストール

--- src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
@@ -6,3 +6,4 @@ mkdir /home/rails/.config
 cp -R helpers/installers/config-nvim /home/rails/.config/nvim
 
 su - rails -c 'nvim --headless "+Lazy! sync" +qa'
+su - rails -c 'nvim --headless "+TSInstall! all" +qa'
