7e5d223cff9a01f83d7f445caf3661b7905a3d02
Koichi Takabatake <takabatakekoichi@gmail.com>
2024-02-22 19:58:22 +0900

    permission

--- src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
@@ -4,8 +4,12 @@ set -eux
 
 su - neovimuser -c 'mkdir /home/neovimuser/.config'
 cp -R helpers/installers/config-nvim /home/neovimuser/.config/nvim
-chown neovimuser -R /home/neovimuser/.config/nvim
+chown neovimuser:neovimuser -R /home/neovimuser/.config/nvim
 
 su - neovimuser -c 'nvim --headless "+Lazy! sync" +qa'
-su - neovimuser -c 'nvim --headless "+TSInstallSync! all" +qa'
+rm -rf /usr/local/bundle
+mkdir /usr/local/bundle
+chown neovimuser:neovimuser -R /usr/local/bundle
+chown neovimuser:neovimuser -R /usr/local/lib/ruby
+su - neovimuser -c 'cd /app && bundle install --clean'
 updatedb
