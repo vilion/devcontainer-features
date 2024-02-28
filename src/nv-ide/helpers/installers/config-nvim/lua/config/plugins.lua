4386343839612520dae6ba4ad932b27b79955253
Koichi Takabatake <takabatakekoichi@gmail.com>
2024-02-28 11:30:44 +0900

    yard gems rbs dbt lsp

--- src/nv-ide/helpers/installers/config-nvim/lua/config/plugins.lua
+++ src/nv-ide/helpers/installers/config-nvim/lua/config/plugins.lua
@@ -587,10 +587,109 @@ return {
         sources = {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.diagnostics.sqlfluff,
           null_ls.builtins.completion.spell,
         },
       })
     end,
     dependencies = {"nvim-lua/plenary.nvim", "vim-test/vim-test"}
+  },
+  {
+   'sindrets/diffview.nvim',
+    lazy = false,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    lazy = false,
+  },
+  {
+    "rshkarin/mason-nvim-lint",
+    event = 'VeryLazy',
+    lazy = true,
+    dependencies = {"mason.nvim", "mfussenegger/nvim-lint"},
+    opts = {
+      -- Event to trigger linters
+      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+      linters_by_ft = {
+        sql = { "sqlfluff" },
+        -- Use the "*" filetype to run linters on all filetypes.
+        -- ['*'] = { 'global linter' },
+        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        -- ['_'] = { 'fallback linter' },
+      },
+      linters = {}
+    },
+    config = function(_, opts)
+    local M = {}
+    -- local rubocop = require("lint").linters.rubocop
+    -- rubocop.cmd = 'bundle exec rubocop'
+    -- rubocop.args = {'--lsp'}
+    -- rubocop.stdin = false
+
+    local lint = require("lint")
+    for name, linter in pairs(opts.linters) do
+      if type(linter) == "table" and type(lint.linters[name]) == "table" then
+        lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
+      else
+        lint.linters[name] = linter
+      end
+    end
+
+    local rubocop = lint.linters.rubocop
+    print(rubocop.cmd)
+    print(rubocop.args[0])
+    print(rubocop.args[1])
+    print(rubocop.args[2])
+    print(rubocop.args[3])
+    print(rubocop.args[4])
+
+    lint.linters_by_ft = opts.linters_by_ft
+
+    function M.debounce(ms, fn)
+      local timer = vim.loop.new_timer()
+      return function(...)
+        local argv = { ... }
+        timer:start(ms, 0, function()
+          timer:stop()
+          vim.schedule_wrap(fn)(unpack(argv))
+        end)
+      end
+    end
+
+    function M.lint()
+      local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+
+      -- Add fallback linters.
+      if #names == 0 then
+        vim.list_extend(names, lint.linters_by_ft["_"] or {})
+      end
+
+      -- Add global linters.
+      vim.list_extend(names, lint.linters_by_ft["*"] or {})
+
+      -- Filter out linters that don't exist or don't match the condition.
+      local ctx = { filename = vim.api.nvim_buf_get_name(0) }
+      ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+      names = vim.tbl_filter(function(name)
+        local linter = lint.linters[name]
+        if not linter then
+          print("Linter not found: " .. name)
+        end
+        return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
+      end, names)
+
+      -- Run linters.
+      if #names > 0 then
+        lint.try_lint(names)
+      end
+    end
+
+    vim.api.nvim_create_autocmd(opts.events, {
+      group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+      callback = M.debounce(100, M.lint),
+    })
+
+      -- require('mason-nvim-lint').setup()
+  end,
   }
 }
