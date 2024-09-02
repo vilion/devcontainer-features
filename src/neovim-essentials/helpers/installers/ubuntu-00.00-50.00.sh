2fb93cb2d4408064d409538f60aa91cb993c1295
vilion <takabatakekoichi@gmail.com>
2024-09-03 08:12:50 +0900

    :book:１０にあげた。yazi対応

--- src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
@@ -44,6 +44,12 @@ if [ $UNZIP = "true" ]; then
 	pkgs+=("unzip")
 fi
 
+wget https://github.com/git/git/archive/refs/tags/v2.46.0.tar.gz \
+	&& tar -xzf v2.46.0.tar.gz \
+	&& cd git-* \
+	&& make prefix=/usr/local all \
+	&& make prefix=/usr/local install
+
 apt install -y "${pkgs[@]}"
 curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
 apt-get update
@@ -57,12 +63,9 @@ install lazygit /usr/local/bin
 adduser --uid 1000 neovimuser
 
 pip3 install pynvim
-if command -v gem
-then
-	gem install neovim
-fi
 npm install -g neovim
 npm install -g @fivetrandevelopers/dbt-language-server
+npm install -g tree-sitter-cli
 apt-get install fd-find less
 apt-get install mlocate
 
@@ -77,13 +80,21 @@ apt-get install -y gettext \
     libxtst-dev \
     libxt-dev \
     libsm-dev \
-    libxpm-dev
+    libxpm-dev \
+    openssh-server
 
 apt install xsel xclip wl-clipboard -y
 apt-get install -y xsel xclip wl-clipboard
 
- wget https://github.com/git/git/archive/refs/tags/v2.45.1.tar.gz \
-    && tar -xzf v2.45.1.tar.gz \
-    && cd git-* \
-    && make prefix=/usr/local all \
-    && make prefix=/usr/local install
+su -l neovimuser << 'EOF'
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH=$PATH:$HOME/.cargo/bin
+rustup update
+cargo install --locked --git https://github.com/sxyazi/yazi.git yazi-fm yazi-cli
+if command -v gem
+then
+	gem install neovim
+fi
+EOF
+
+
