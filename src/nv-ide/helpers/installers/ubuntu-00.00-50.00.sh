86e68c663cb7586a9cebb22aa62159ad5890e123
vilion <takabatakekoichi@gmail.com>
2024-09-24 00:17:48 +0900

    glow markdown

--- src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
@@ -44,3 +44,8 @@ then
 	updatedb
 fi
 apt-get install -y default-mysql-server
+cd /tmp
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | tee /etc/apt/sources.list.d/charm.list
+apt update -y && apt install glow -y
