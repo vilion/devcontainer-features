0e7a49a38a89e98693f692e55a14d74c8883a244
vilion <takabatakekoichi@gmail.com>
2024-10-12 19:57:47 +0900

    installなど同期で実行

--- src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
@@ -37,10 +37,10 @@ then
 	su neovimuser -c 'cd /app && git config --global --add safe.directory /app'
 	su neovimuser -c 'cd /app && git config --global user.email "takabatakekoichi@gmail.com"'
 	su neovimuser -c 'cd /app && git config --global user.name "takabatake"'
-	su neovimuser -c 'cd /app && bundle install'
-	su neovimuser -c 'cd /app && gem install ruby-lsp ruby-lsp-rails ruby-lsp-rspec neovim rbs'
-	su neovimuser -c 'cd /app && rbs collection install'
-	su neovimuser -c 'cd /app && yard gems'
+	su neovimuser -c 'cd /app && bundle install & wait'
+	su neovimuser -c 'cd /app && gem install ruby-lsp ruby-lsp-rails ruby-lsp-rspec neovim rbs & wait'
+	su neovimuser -c 'cd /app && rbs collection install & wait'
+	su neovimuser -c 'cd /app && yard gems & wait'
 fi
 
 if command -v updatedb
