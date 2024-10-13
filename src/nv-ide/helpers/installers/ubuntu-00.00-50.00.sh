6a191fc52927bdea0de066828e5cf47016bbe9b7
vilion <takabatakekoichi@gmail.com>
2024-10-13 14:59:25 +0900

    glowのインストールを断念

--- src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/nv-ide/helpers/installers/ubuntu-00.00-50.00.sh
@@ -52,4 +52,4 @@ cd /tmp
 # mkdir -p /etc/apt/keyrings
 # curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --no-tty --dearmor -o /etc/apt/keyrings/charm.gpg
 # echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | tee /etc/apt/sources.list.d/charm.list
-apt update -y && apt install glow -y
+# apt update -y && apt install glow -y
