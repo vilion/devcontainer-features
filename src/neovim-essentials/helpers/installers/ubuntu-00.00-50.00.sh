da05e6f86964a9816fcc969e86606c159beb16fc
vilion <takabatakekoichi@gmail.com>
2024-10-13 12:16:51 +0900

    luarocksのミス修正。jsregexpも試しに入れてみる

--- src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
@@ -36,10 +36,8 @@ if [ $RIPGREP = "true" ]; then
 	pkgs+=("ripgrep")
 fi
 
-if [ $UNZIP = "true" ]; then
-	pkgs+=("unzip")
-fi
-
+pkgs+=("unzip")
+pkgs+=("libreadline-dev")
 pkgs+=("lua5.4")
 pkgs+=("fzf")
 pkgs+=("luajit")
@@ -59,11 +57,11 @@ apt install -y "${pkgs[@]}"
 wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz \
 	&& tar -xzpf luarocks-3.11.1.tar.gz \
 	&& cd luarocks-3.11.1 \
-	&& ./configure --with-lua-include=/usr/include/lua5.1 --with-lua-bin=/usr/bin \
+	&& ./configure --with-lua-include=/usr/include/lua5.4 --with-lua-bin=/usr/bin \
         && make \
         && make install
 
-luarocks config variables.LUA_INCDIR /usr/include/lua5.1
+luarocks config variables.LUA_INCDIR /usr/include/lua5.4
 # luarocks install jsregexp
 
 cd /tmp
