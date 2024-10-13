dfdae9820ef39d3e5e3472ba37d3d4c97dfe71f2
vilion <takabatakekoichi@gmail.com>
2024-10-13 12:16:51 +0900

    luarocksのミス修正。jsregexpも試しに入れてみる

--- src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
@@ -59,12 +59,12 @@ apt install -y "${pkgs[@]}"
 wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz \
 	&& tar -xzpf luarocks-3.11.1.tar.gz \
 	&& cd luarocks-3.11.1 \
-	&& ./configure --with-lua-include=/usr/include/lua5.1 --with-lua-bin=/usr/bin \
+	&& ./configure --with-lua-include=/usr/include/lua5.4 --with-lua-bin=/usr/bin \
         && make \
         && make install
 
-luarocks config variables.LUA_INCDIR /usr/include/lua5.1
-# luarocks install jsregexp
+luarocks config variables.LUA_INCDIR /usr/include/lua5.4
+luarocks install jsregexp
 
 cd /tmp
 rm -rf /tmp/ctags
