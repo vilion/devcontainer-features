d37b918231677c3f643ca3ea7a6891cdecc78ae0
vilion <4006264+vilion@users.noreply.github.com>
2025-01-02 23:19:14 +0900

    fastfetch

--- src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/neovim-essentials/helpers/installers/ubuntu-00.00-50.00.sh
@@ -60,6 +60,11 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 cd /tmp
 apt install -y "${pkgs[@]}"
+apt-get install software-properties-common -y
+add-apt-repository ppa:zhangsongcui3371/fastfetch
+apt update -y
+apt install -y fastfetch
+
 wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz \
 	&& tar -xzpf luarocks-3.11.1.tar.gz \
 	&& cd luarocks-3.11.1 \
